91412647463f32737c97b72b11635a34eaa76a3b
Tim Visée <timvisee@gmail.com>
2016-11-18 18:46:55 +0100

    Test build using xbuild and install using NuGet

--- ci_build.ps1
+++ ci_build.ps1
@@ -1,4 +1,4 @@
-#log the runtimes
+# Log the runtimes
 dnvm list
 dnvm install 1.0.0-rc1-update1 -r coreclr -a x86
 dnvm install 1.0.0-rc1-update1 -r clr -a x86
@@ -9,13 +9,23 @@ dnvm use 1.0.0-rc1-update1 -a x64 -r coreclr
 dnu feeds list
 dnu restore
 
-#run the build
+// Report mono and xbuild versions
+& "C:\Program Files (x86)\Mono\bin\mono" /version
+& "C:\Program Files (x86)\Mono\bin\xbuild" /version
+
+# Restore NuGet configuration
+nuget restore hhs-p6-webshop-project.sln
+
+# Build the actual project using xbuild
+& "C:\Program Files (x86)\Mono\bin\xbuild" /p:Configuration=Release hhs-p6-webshop-project.sln
+
+# Run the build
 & "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" hhs-p6-webshop-project.sln
 
-##tests
+## Tests
 #dnvm use 1.0.0-rc1-update1 -a x64 -r coreclr
 #dnx -p tests\hhs-p6-webshop-project.Tests test -xml xunit-results.xml
 
-## upload results to AppVeyor
+## Upload results to AppVeyor
 #$wc = New-Object 'System.Net.WebClient'
 #$wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\xunit-results.xml))
