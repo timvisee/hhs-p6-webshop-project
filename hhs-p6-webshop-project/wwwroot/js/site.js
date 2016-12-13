6ca56834df8116c19da07ed956dea8163ffc9595
timvisee <timvisee@gmail.com>
2016-12-13 16:45:54 +0100

    Move scripts to JavaScript files, slightly improve search field script

--- hhs-p6-webshop-project/wwwroot/js/site.js
+++ hhs-p6-webshop-project/wwwroot/js/site.js
@@ -1,1 +1,12 @@
-// Write your Javascript code.
+// Run this code when the page is finished loading
+$(document).ready(function() {
+
+    // Toggle the search box when the search button is clicked
+    $("#header_search_toggle").click(function() {
+        // Make the search box and field visible
+        $("#search_box").animate({width: 'toggle'}, 350, function() {
+            // Focus the search field
+            $("#header_search").focus();
+        });
+    });
+});
