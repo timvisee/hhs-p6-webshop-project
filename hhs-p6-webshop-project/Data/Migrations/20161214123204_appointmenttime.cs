174ee29f9e1c3c9b081404e347100c6aa214ce74
Dennis Volkering <dennisvolkering@hotmail.com>
2016-12-14 15:03:38 +0100

    Added appointmentTimes views and controller

---
+++ hhs-p6-webshop-project/Data/Migrations/20161214123204_appointmenttime.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace hhs_p6_webshop_project.Data.Migrations
+{
+    public partial class appointmenttime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
