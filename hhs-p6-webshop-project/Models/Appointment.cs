21d73448eb4abf26deba199610b2f127e7a953a7
Dennis Volkering <dennisvolkering@hotmail.com>
2016-12-14 12:14:58 +0100

    (Rdoerick) Fixed EntityFramework

---
+++ hhs-p6-webshop-project/Models/Appointment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hhs_p6_webshop_project.Models {
+    public class Appointment {
+        public int ID { get; set; }
+        public string Mail { get; set; }
+        public DateTime DateMarried { get; set; }
+        public AppointmentTime AppointmentTime { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public bool Confirmation { get; set; }
+    }
+}
---
+++ hhs-p6-webshop-project/Models/AppointmentTime.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hhs_p6_webshop_project.Models {
+    public class AppointmentTime {
+        public int ID { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
