fbbd4267b4665c829aaff87fe5b500d57419df8e
Tim Visée <timvisee@gmail.com>
2016-12-14 20:25:09 +0100

    Improve logging and add initialization script

--- ci/linux/ci.sh
+++ ci/linux/ci.sh
@@ -1,16 +1,20 @@
 # Fancy logging function
 log() {
-    printf "\n%s\n" "----------------------------------------------------------------"
+    printf "\n\n%s\n" "----------------------------------------------------------------"
     printf "$1"
-    printf "\n%s\n" "----------------------------------------------------------------"
+    printf "\n%s\n\n" "----------------------------------------------------------------"
 }
+# Set the proper execution permissions for the scripts
+sudo chmod 777 ./init.sh
+sudo chmod 777 ./setup.sh
+sudo chmod 777 ./build.sh
+sudo chmod 777 ./test.sh
+
+# Initialize
+./init.sh
 
 # We're starting, show a status message
 log "Starting CI build progress..."
-
-# Set the proper execution permissions for the scripts
-sudo chmod 777 ./setup.sh
-sudo chmod 777 ./build.sh
 
 # Set up the build environment
 ./setup.sh
---
+++ ci/linux/init.sh
@@ -0,0 +1,6 @@
+# Initialization message
+echo "----------------------------------------------------------------"
+echo "  dotnet core CI build tool for Linux"
+echo "  Written by Tim Visee, timvisee.com"
+echo "----------------------------------------------------------------"
+echo ""
