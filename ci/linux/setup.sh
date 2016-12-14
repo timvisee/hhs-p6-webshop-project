1ff118e41ea2d67ab237b2662aa0d848e5b591e8
Tim Visée <timvisee@gmail.com>
2016-12-14 20:46:31 +0100

    Install bower/gulp, download web dependencies, minify sources

--- ci/linux/setup.sh
+++ ci/linux/setup.sh
@@ -22,9 +22,30 @@ sudo apt-get update
 log "Installing dotnet..."
 sudo apt-get -y install dotnet-dev-1.0.0-preview2.1-003177
 
+# Install bower
+log "Installing bower..."
+sudo npm install -g bower
+
+# Install gulp
+log "Installing gulp..."
+sudo npm install -g gulp
+
 # Install project dependencies
 log "Installing project dependencies..."
 dotnet restore
+
+# Installing NPM depenendencies
+log "Installing NPM dependencies..."
+sudo npm install
+
+# Install bower dependencies
+log "Installing bower dependencies..."
+sudo bower install
+
+# Minify using gulp
+log "Cleaning and minifying using gulp..."
+gulp clean
+gulp min
 
 # Build environment configured
 log "Build environment configured."
