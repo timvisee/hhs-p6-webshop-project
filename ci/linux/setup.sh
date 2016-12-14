092a66c3f424a8646583150dbddf7507ce6306c1
Tim Visée <timvisee@gmail.com>
2016-12-14 20:54:05 +0100

    Manually install NPM with NodeJS

--- ci/linux/setup.sh
+++ ci/linux/setup.sh
@@ -14,6 +14,11 @@ log "Setting up build environment...\nSetting up dotnet repository..."
 sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
 
+# Set up the NPM repository
+log "Setting up NPM repository..."
+sudo apt-get install python-software-properties
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+
 # Update all repositories
 log "Updating all repositories..."
 sudo apt-get update
@@ -21,6 +26,10 @@ sudo apt-get update
 # Install dotnet
 log "Installing dotnet..."
 sudo apt-get -y install dotnet-dev-1.0.0-preview2.1-003177
+
+# Install NodeJS with NPM
+log "Installing NodeJS with NPM..."
+sudo apt-get install -y nodejs
 
 # Install bower
 log "Installing bower..."
