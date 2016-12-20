6a34655e81d846b05f72a2cdc5c37e724f1c9197
timvisee <timvisee@gmail.com>
2016-12-20 12:59:52 +0100

    Move routing logic to new class

---
+++ hhs-p6-webshop-project/App/Router.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace hhs_p6_webshop_project.App {
+    public class Router {
+
+        /// <summary>
+        /// Set up the router for MVC.
+        /// </summary>
+        /// <param name="app">Application builder instance to set up the routes for.</param>
+        public void SetUp(IApplicationBuilder app) {
+            // Configure the MVC routes
+            app.UseMvc(ConfigureRoutes());
+        }
+
+        /// <summary>
+        /// Configure the actual routes, as an action with route builders.
+        /// </summary>
+        /// <returns>Action with route builders</returns>
+        private static Action<IRouteBuilder> ConfigureRoutes() {
+            return routes => {
+                // Default router
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            };
+        }
+    }
+}
