58945719ec46e41c2614cf5357cdd434f5899d0f
timvisee <timvisee@gmail.com>
2016-12-20 15:14:56 +0100

    Use dictionaries, create dictionary merge method, remove object system

--- hhs-p6-webshop-project/App/Ajax/AjaxResponse.cs
+++ hhs-p6-webshop-project/App/Ajax/AjaxResponse.cs
@@ -31,48 +31,45 @@ namespace hhs_p6_webshop_project.App.Ajax {
         }
 
         /// <summary>
+        /// Create a dictionary for this AJAX response.
+        /// </summary>
+        /// <returns>Dictionary.</returns>
+        public Dictionary<string, object> ToDictionary() {
+            // Create a base dictionary
+            Dictionary<string, object> Base = new Dictionary<string, object>();
+
+            // Merge the data
+            MergeDictionary(Base, Data);
+
+            // Set the status
+            Base.Add("status", ErrorStatus == null ? "ok" : "err");
+            if (ErrorStatus != null)
+                Base.Add("error", ErrorStatus.ToDictionary());
+
+            // Return the dictionary
+            return Base;
+        }
+
+        /// <summary>
         /// Format the AJAX response to Json data, used to send to the client.
         /// </summary>
         /// <returns>Json formatted AJAX result.</returns>
         public JsonResult FormatJson() {
-            // Create a new base object
-            // TODO: The base object is empty here. Fill it immediately for better performance.
-            Object result = new {
-                status = ErrorStatus == null
-            };
-
-            // Add the error response if set
-            if (ErrorStatus != null) {
-                MergeObjects(result, ErrorStatus.AsObject());
-            }
-
-            // TODO: Respond with the actual data here!
-            return new JsonResult(result);
+            return new JsonResult(ToDictionary());
         }
 
         /// <summary>
-        /// Copy the properties of the given source, to the given target.
-        /// This can be used to merge objects.
+        /// Merge two dictionaries.
         /// </summary>
-        /// <param name="target">Target object.</param>
-        /// <param name="source">Source object.</param>
-        /// <typeparam name="T">Object type.</typeparam>
-        /// <returns>Target object, may be used for method chaining.</returns>
-        public static T MergeObjects<T>(T target, T source) {
-            // Get the actual type
-            Type t = typeof(T);
+        /// <param name="target">Target dictionary.</param>
+        /// <param name="source">Source dictionary.</param>
+        /// <returns>Target dictionary.</returns>
+        public static Dictionary<string, object> MergeDictionary(Dictionary<string, object> target,
+            Dictionary<string, object> source) {
+            // Merge the two dictionaries
+            source.ToList().ForEach(x => target.Add(x.Key, x.Value));
 
-            // Fetch the properties
-            var properties = t.GetProperties().Where(prop => prop.CanRead && prop.CanWrite);
-
-            // Copy the source values to the target
-            foreach (var prop in properties) {
-                var value = prop.GetValue(source, null);
-                if (value != null)
-                    prop.SetValue(target, value, null);
-            }
-
-            // Return the target, for method chaining
+            // Return the target
             return target;
         }
 
