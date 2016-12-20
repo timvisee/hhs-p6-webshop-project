d01fc204202a4ac220188c7fbff9b84fe7fbc7b0
timvisee <timvisee@gmail.com>
2016-12-20 14:27:05 +0100

    Create not found AJAX error

---
+++ hhs-p6-webshop-project/App/Ajax/NotFoundErrorStatus.cs
@@ -0,0 +1,19 @@
+﻿namespace hhs_p6_webshop_project.App.Ajax {
+    public class NotFoundErrorStatus : ErrorStatus {
+
+        /// <summary>
+        /// Error message.
+        /// </summary>
+        public const string ErrorMessage = "Endpoint not found.";
+
+        /// <summary>
+        /// Error code.
+        /// </summary>
+        public const int ErrorCode = 1;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public NotFoundErrorStatus() : base(ErrorMessage, ErrorCode) {}
+    }
+}
