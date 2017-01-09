d2b644bc70f9b8818367edcd597cf1dc42e250d4
Dennis Volkering <dennisvolkering@hotmail.com>
2017-01-09 12:10:24 +0100

    Added product models

---
+++ hhs-p6-webshop-project/Models/ProductModels/PropertyType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hhs_p6_webshop_project.Models.ProductModels {
+    public class PropertyType {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String DataType { get; set; }
+        public Boolean Required { get; set; }
+        public Boolean Multiple { get; set; }
+        public Boolean AllowCustom { get; set; }
+    }
+}
