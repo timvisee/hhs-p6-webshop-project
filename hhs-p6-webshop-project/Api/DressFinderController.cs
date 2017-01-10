349d0807abce21d2ad4132930ecbc37de286283a
TheBauwssss <github@rick-soft.com>
2017-01-10 13:21:13 +0100

    Added product generation to database builder Extended product service Added indented json option Started work on Ajax API for dressfinder Started work on paged responses (still bugged)

---
+++ hhs-p6-webshop-project/Api/DressFinderController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using hhs_p6_webshop_project.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hhs_p6_webshop_project.Api
+{
+    [Route("api/dressfinder")]
+    public class DressFinderController : Controller
+    {
+        public IProductService ProductService { get;set; }
+
+        public DressFinderController(IProductService productService) {
+            ProductService = productService;
+        }
+
+        [HttpGet("product/all")]
+        public JsonResult GetAllProducts(int start, int count) {
+            return Json(ProductService.GetAllProducts());
+        }
+
+        [HttpGet("product/{start}/{count}")]
+        public JsonResult GetAllProductsPaged(int start, int count) {
+            return Json(ProductService.GetAllProductsPaged(start, count));
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- hhs-p6-webshop-project/Services/IProductService.cs
+++ hhs-p6-webshop-project/Services/IProductService.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using hhs_p6_webshop_project.Api;
 using hhs_p6_webshop_project.Models.ProductModels;
 
 namespace hhs_p6_webshop_project.Services
 {
     public interface IProductService {
+        /// <summary>
+        /// Returns a list of all products, sorted by Id, ascending.
+        /// </summary>
+        /// <returns>A <see cref="List{T}"/> of all <see cref="Product"/> product instances.</returns>
+        List<Product> GetAllProducts();
+
+        /// <summary>
+        /// Returns a list of all products, sorted by Id, ascending.
+        /// </summary>
+        /// <returns>A <see cref="List{T}"/> of all <see cref="Product"/> product instances.</returns>
+        PagedResponse GetAllProductsPaged(int start, int count);
+
+        /// <summary>
+        /// Returns an <see cref="ArrayList"/> of all available <see cref="PropertyValue"/> for a given range of <see cref="Product"/>.
+        /// </summary>
+        /// <param name="products">the <see cref="Product"/> instances to return the available filters for</param>
+        /// <returns>A <see cref="List{T}"/> of available filters for a given <see cref="Product"/> range</returns>
+        List<PropertyValue> GetAllAvailableFiltersForProducts(List<Product> products);
+
         List<PropertyType> GetPropertyTypesForProduct(Product product);
         List<PropertyType> AllProductFilterTypesAsList();
     }
