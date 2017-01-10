b41d3b6aa1ae5f907b04a4cef8f42ae97c03e57d
Tim Visée <timvisee@gmail.com>
2017-01-10 13:28:15 +0100

    Implement equality method for PorpertyType model

--- hhs-p6-webshop-project/Models/ProductModels/PropertyType.cs
+++ hhs-p6-webshop-project/Models/ProductModels/PropertyType.cs
@@ -4,15 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace hhs_p6_webshop_project.Models.ProductModels {
-    public class PropertyType : IEquatable<PropertyType> {
+    public class PropertyType {
+
         public int ID { get; set; }
         public string Name { get; set; }
         public string DataType { get; set; }
         public bool Required { get; set; }
         public bool Multiple { get; set; }
         public bool AllowCustom { get; set; }
-        public bool Equals(PropertyType other) {
-            return ID == other.ID;
+
+        /// <summary>
+        /// Compare this property type to another property type instance.
+        /// </summary>
+        /// <param name="other">Other property type instance to compare to.</param>
+        /// <returns>True if the types are equal, false if not.</returns>
+        public bool Equals(object other) {
+            // Return false if the object isn't a PropertyType isntance
+            if(other.GetType() != typeof(PropertyType))
+                return false;
+
+            // Get the other property type
+            PropertyType otherType = (PropertyType) other;
+
+            // Compare the IDs and return the result
+            return ID == otherType.ID;
         }
+
     }
 }
