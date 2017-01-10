4be47f24a4288a6db03b1efa0bd3889e530ba92c
Tim Visée <timvisee@gmail.com>
2017-01-10 14:28:31 +0100

    Create GetProductsFiltered method (WIP)

--- hhs-p6-webshop-project/Services/ProductFilterService.cs
+++ hhs-p6-webshop-project/Services/ProductFilterService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using hhs_p6_webshop_project.Data;
 using hhs_p6_webshop_project.Models.ProductModels;
 using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
 
 namespace hhs_p6_webshop_project.Services {
 
@@ -16,9 +17,29 @@ namespace hhs_p6_webshop_project.Services {
             DatabaseContext = dbContext;
         }
 
+        /// <summary>
+        /// Get a list of products filtered by the given property values.
+        /// </summary>
+        /// <param name="values">Property values to filter on.</param>
+        /// <returns>List of products. An empty list of products might be returned if the filters aren't consistent.</returns>
+        public List<Product> GetProductsFiltered(List<PropertyValue> values) {
+            // Get the queryable products
+            IQueryable<Product> queryable = DatabaseContext.Product.AsQueryable();
+            IQueryable<PropertyValue> queryableValue = DatabaseContext.PropertyValue.AsQueryable();
+
+            // Get a list of property types that are used as filters
+            HashSet<PropertyType> types = new HashSet<PropertyType>();
+            values.ForEach(value => types.Add(value.PropertyType));
+
+            // TODO: Unfinished! Complete this method.
+
+            // Return the list of products
+            return queryable.ToList();
+        }
+
         public List<PropertyType> GetPropertyTypesForProduct(Product product) {
             return product.Properties.Select(type => type.PropertyType).Distinct().ToList();
-        } 
+        }
 
         // TODO: Can't we fetch all types from the ProductType model table here?
         public List<PropertyType> AllProductFilterTypesAsList() {
