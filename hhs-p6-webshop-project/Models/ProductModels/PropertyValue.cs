437c0fc9e297dc7d51139dac3dc10b86054eeafc
Tim Visée <timvisee@gmail.com>
2017-01-10 14:28:31 +0100

    Create GetProductsFiltered method (WIP)

--- hhs-p6-webshop-project/Models/ProductModels/PropertyValue.cs
+++ hhs-p6-webshop-project/Models/ProductModels/PropertyValue.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace hhs_p6_webshop_project.Models.ProductModels {
     public class PropertyValue : IEquatable<PropertyValue> {
