c0fbfbcbabb3af863e4712d38ab1907fca0c2122
TheBauwssss <github@rick-soft.com>
2017-01-12 16:08:52 +0100

    Implemented filter parsing Implemented product filter Extended ProductService

--- hhs-p6-webshop-project/Services/IProductService.cs
+++ hhs-p6-webshop-project/Services/IProductService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using hhs_p6_webshop_project.Api;
+using hhs_p6_webshop_project.ExtraModels;
 using hhs_p6_webshop_project.Models.ProductModels;
 
 namespace hhs_p6_webshop_project.Services
@@ -15,7 +16,17 @@ namespace hhs_p6_webshop_project.Services
         /// <returns>A <see cref="List{T}"/> of all <see cref="Product"/> product instances.</returns>
         List<Product> GetAllProducts();
 
+        /// <summary>
+        /// Returns a list of all PropertyTypes with all possible values.
+        /// </summary>
+        /// <returns></returns>
+        List<ProductFilter> GetAllProductFilters();
+            
         List<PropertyValueCoupling> Test();
+
+        List<ProductFilter> ParseFilterRequest(FilterRequest req);
+
+        List<Product> Filter(List<ProductFilter> filters, List<Product> products);
 
             /// <summary>
         /// Returns a list of all products, sorted by Id, ascending.
