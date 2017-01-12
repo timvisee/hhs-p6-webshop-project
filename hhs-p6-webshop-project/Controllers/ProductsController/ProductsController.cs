af31f1f273b5e2f838ad2e5f9bfbad4680f9d715
Dennis Volkering <dennisvolkering@hotmail.com>
2017-01-12 13:57:58 +0100

    Get all the products from productservice

--- hhs-p6-webshop-project/Controllers/ProductsController/ProductsController.cs
+++ hhs-p6-webshop-project/Controllers/ProductsController/ProductsController.cs
@@ -7,35 +7,31 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using hhs_p6_webshop_project.Data;
 using hhs_p6_webshop_project.Models.ProductModels;
+using hhs_p6_webshop_project.Services;
 
-namespace hhs_p6_webshop_project.Controllers.ProductsController
-{
-    public class ProductsController : Controller
-    {
+namespace hhs_p6_webshop_project.Controllers.ProductsController {
+    public class ProductsController : Controller {
         private readonly ApplicationDbContext _context;
+        private readonly IProductService _productService;
 
-        public ProductsController(ApplicationDbContext context)
-        {
-            _context = context;    
+        public ProductsController(ApplicationDbContext context, IProductService productService) {
+            _context = context;
+            _productService = productService;
         }
 
         // GET: Products
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Product.ToListAsync());
+        public async Task<IActionResult> Index() {
+            return View(_productService.GetAllProducts());
         }
 
         // GET: Products/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
+        public async Task<IActionResult> Details(int? id) {
+            if (id == null) {
                 return NotFound();
             }
 
             var product = await _context.Product.SingleOrDefaultAsync(m => m.ProductId == id);
-            if (product == null)
-            {
+            if (product == null) {
                 return NotFound();
             }
 
@@ -43,8 +39,7 @@ namespace hhs_p6_webshop_project.Controllers.ProductsController
         }
 
         // GET: Products/Create
-        public IActionResult Create()
-        {
+        public IActionResult Create() {
             return View();
         }
 
@@ -53,10 +48,8 @@ namespace hhs_p6_webshop_project.Controllers.ProductsController
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ProductId,Description,Name")] Product product)
-        {
-            if (ModelState.IsValid)
-            {
+        public async Task<IActionResult> Create([Bind("ProductId,Description,Name")] Product product) {
+            if (ModelState.IsValid) {
                 _context.Add(product);
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Index");
@@ -65,16 +58,13 @@ namespace hhs_p6_webshop_project.Controllers.ProductsController
         }
 
         // GET: Products/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
+        public async Task<IActionResult> Edit(int? id) {
+            if (id == null) {
                 return NotFound();
             }
 
             var product = await _context.Product.SingleOrDefaultAsync(m => m.ProductId == id);
-            if (product == null)
-            {
+            if (product == null) {
                 return NotFound();
             }
             return View(product);
@@ -85,28 +75,19 @@ namespace hhs_p6_webshop_project.Controllers.ProductsController
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ProductId,Description,Name")] Product product)
-        {
-            if (id != product.ProductId)
-            {
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,Description,Name")] Product product) {
+            if (id != product.ProductId) {
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
+            if (ModelState.IsValid) {
+                try {
                     _context.Update(product);
                     await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProductExists(product.ProductId))
-                    {
+                } catch (DbUpdateConcurrencyException) {
+                    if (!ProductExists(product.ProductId)) {
                         return NotFound();
-                    }
-                    else
-                    {
+                    } else {
                         throw;
                     }
                 }
@@ -116,16 +97,13 @@ namespace hhs_p6_webshop_project.Controllers.ProductsController
         }
 
         // GET: Products/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
+        public async Task<IActionResult> Delete(int? id) {
+            if (id == null) {
                 return NotFound();
             }
 
             var product = await _context.Product.SingleOrDefaultAsync(m => m.ProductId == id);
-            if (product == null)
-            {
+            if (product == null) {
                 return NotFound();
             }
 
@@ -135,16 +113,14 @@ namespace hhs_p6_webshop_project.Controllers.ProductsController
         // POST: Products/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
+        public async Task<IActionResult> DeleteConfirmed(int id) {
             var product = await _context.Product.SingleOrDefaultAsync(m => m.ProductId == id);
             _context.Product.Remove(product);
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
 
-        private bool ProductExists(int id)
-        {
+        private bool ProductExists(int id) {
             return _context.Product.Any(e => e.ProductId == id);
         }
     }
