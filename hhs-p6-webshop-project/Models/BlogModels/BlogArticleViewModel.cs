f0e067e6265451d404b57ca24b17c7d62fd0709c
Dennis Volkering <dennisvolkering@hotmail.com>
2017-01-13 15:01:44 +0100

    Added blog controllers and models

---
+++ hhs-p6-webshop-project/Models/BlogModels/BlogArticleViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using hhs_p6_webshop_project.Models.BlogModels;
+
+namespace hhs_p6_webshop_project.Models.BlogModels {
+    public class BlogArticleViewModel {
+        public BlogArticleViewModel() {
+            BlogCategories = new List<BlogCategory>();
+            BlogArticle = new BlogArticle();
+        }
+
+        public List<BlogCategory> BlogCategories { get; set; }
+        public BlogArticle BlogArticle { get; set; }
+
+    }
+}
