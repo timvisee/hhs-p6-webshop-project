4c66d455ddf33f3846d5237d8f98809b05dac980
kverkade <verkaduh@gmail.com>
2017-01-16 16:22:35 +0100

    Fixed uploading images in ProductImagesController

--- hhs-p6-webshop-project/Controllers/ProductControllers/ProductImagesController.cs
+++ hhs-p6-webshop-project/Controllers/ProductControllers/ProductImagesController.cs
@@ -7,36 +7,31 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using hhs_p6_webshop_project.Data;
 using hhs_p6_webshop_project.Models.ProductModels;
+using Microsoft.AspNetCore.Http;
+using System.IO;
 
-namespace hhs_p6_webshop_project.Controllers.ProductControllers
-{
-    public class ProductImagesController : Controller
-    {
+namespace hhs_p6_webshop_project.Controllers.ProductControllers {
+    public class ProductImagesController : Controller {
         private readonly ApplicationDbContext _context;
 
-        public ProductImagesController(ApplicationDbContext context)
-        {
-            _context = context;    
+        public ProductImagesController(ApplicationDbContext context) {
+            _context = context;
         }
 
         // GET: ProductImages
-        public async Task<IActionResult> Index()
-        {
+        public async Task<IActionResult> Index() {
             var applicationDbContext = _context.ProductImages.Include(p => p.ColorOption);
             return View(await applicationDbContext.ToListAsync());
         }
 
         // GET: ProductImages/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
+        public async Task<IActionResult> Details(int? id) {
+            if (id == null) {
                 return NotFound();
             }
 
             var productImage = await _context.ProductImages.SingleOrDefaultAsync(m => m.ProductImageId == id);
-            if (productImage == null)
-            {
+            if (productImage == null) {
                 return NotFound();
             }
 
@@ -44,8 +39,7 @@ namespace hhs_p6_webshop_project.Controllers.ProductControllers
         }
 
         // GET: ProductImages/Create
-        public IActionResult Create()
-        {
+        public IActionResult Create() {
             ViewData["ColorOptionId"] = new SelectList(_context.ColorOptions, "ColorOptionId", "Color");
             return View();
         }
@@ -55,10 +49,19 @@ namespace hhs_p6_webshop_project.Controllers.ProductControllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ProductImageId,ColorOptionId,Path")] ProductImage productImage)
-        {
-            if (ModelState.IsValid)
-            {
+        public async Task<IActionResult> Create([Bind("ProductImageId,ColorOptionId,Path")] ProductImage productImage, IFormFile image) {
+            if (ModelState.IsValid || image != null) {
+
+                string filename = ChangePathName(productImage.Path);
+                FileInfo fi = new FileInfo(image.FileName);
+                string extension = fi.Extension;
+                string path = "images/dress/" + filename + extension;
+                productImage.Path = path;
+                using (FileStream fs = System.IO.File.Create("wwwroot/" + path)) {
+                    image.CopyTo(fs);
+                    fs.Flush();
+                }
+
                 _context.Add(productImage);
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Index");
@@ -68,16 +71,13 @@ namespace hhs_p6_webshop_project.Controllers.ProductControllers
         }
 
         // GET: ProductImages/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
+        public async Task<IActionResult> Edit(int? id) {
+            if (id == null) {
                 return NotFound();
             }
 
             var productImage = await _context.ProductImages.SingleOrDefaultAsync(m => m.ProductImageId == id);
-            if (productImage == null)
-            {
+            if (productImage == null) {
                 return NotFound();
             }
             ViewData["ColorOptionId"] = new SelectList(_context.ColorOptions, "ColorOptionId", "Color", productImage.ColorOptionId);
@@ -89,28 +89,30 @@ namespace hhs_p6_webshop_project.Controllers.ProductControllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ProductImageId,ColorOptionId,Path")] ProductImage productImage)
-        {
-            if (id != productImage.ProductImageId)
-            {
+        public async Task<IActionResult> Edit(int id, [Bind("ProductImageId,ColorOptionId,Path")] ProductImage productImage, IFormFile image) {
+            if (id != productImage.ProductImageId) {
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
+            if (ModelState.IsValid || image != null) {
+                try {
+
+                    string filename = ChangePathName(productImage.Path);
+                    FileInfo fi = new FileInfo(image.FileName);
+                    string extension = fi.Extension;
+                    string path = "images/dress/" + filename + extension;
+                    productImage.Path = path;
+                    using (FileStream fs = System.IO.File.Create("wwwroot/" + path)) {
+                        image.CopyTo(fs);
+                        fs.Flush();
+                    }
+
                     _context.Update(productImage);
                     await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProductImageExists(productImage.ProductImageId))
-                    {
+                } catch (DbUpdateConcurrencyException) {
+                    if (!ProductImageExists(productImage.ProductImageId)) {
                         return NotFound();
-                    }
-                    else
-                    {
+                    } else {
                         throw;
                     }
                 }
@@ -121,16 +123,13 @@ namespace hhs_p6_webshop_project.Controllers.ProductControllers
         }
 
         // GET: ProductImages/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
+        public async Task<IActionResult> Delete(int? id) {
+            if (id == null) {
                 return NotFound();
             }
 
             var productImage = await _context.ProductImages.SingleOrDefaultAsync(m => m.ProductImageId == id);
-            if (productImage == null)
-            {
+            if (productImage == null) {
                 return NotFound();
             }
 
@@ -140,17 +139,25 @@ namespace hhs_p6_webshop_project.Controllers.ProductControllers
         // POST: ProductImages/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
+        public async Task<IActionResult> DeleteConfirmed(int id) {
             var productImage = await _context.ProductImages.SingleOrDefaultAsync(m => m.ProductImageId == id);
             _context.ProductImages.Remove(productImage);
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
 
-        private bool ProductImageExists(int id)
-        {
+        private bool ProductImageExists(int id) {
             return _context.ProductImages.Any(e => e.ProductImageId == id);
+        }
+
+        public string ChangePathName(string input) {
+            Guid g = Guid.NewGuid();
+            string GuidString = Convert.ToBase64String(g.ToByteArray());
+            GuidString = GuidString.Replace("=", "");
+            GuidString = GuidString.Replace("+", "");
+            GuidString = GuidString.Replace("/", "");
+
+            return GuidString;
         }
     }
 }
