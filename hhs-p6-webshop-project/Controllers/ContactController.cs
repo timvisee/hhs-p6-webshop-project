5e88ffdd64243c45961b82610863b17238140d00
Miladin Jeremic <miladin@live.nl>
2017-01-16 16:04:28 +0100

    Made progress with contact form mailer

--- hhs-p6-webshop-project/Controllers/ContactController.cs
+++ hhs-p6-webshop-project/Controllers/ContactController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Mail;
+using System.Text;
+using hhs_p6_webshop_project.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace hhs_p6_webshop_project.Controllers
@@ -10,6 +14,43 @@ namespace hhs_p6_webshop_project.Controllers
     {
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactModels c)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage msg = new MailMessage();
+                    SmtpClient smtp = new SmtpClient();
+                    MailAddress from = new MailAddress(c.Email.ToString());
+                    StringBuilder sb = new StringBuilder();
+                    msg.IsBodyHtml = false;
+                    smtp.Host = "mail.yourdomain.com";
+                    smtp.Port = 25;
+                    msg.To.Add("youremail@email.com");
+                    msg.From = from;
+                    msg.Subject = "Contact Us";
+                    sb.Append("First name: " + c.FirstName);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Last name: " + c.LastName);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Email: " + c.Email);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Comments: " + c.Comment);
+                    msg.Body = sb.ToString();
+                    smtp.Send(msg);
+                    msg.Dispose();
+                    return View("Success");
+                }
+                catch (Exception)
+                {
+                    return View("Error");
+                }
+            }
             return View();
         }
     }
---
+++ hhs-p6-webshop-project/Models/ContactModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace hhs_p6_webshop_project.Models
+{
+        public class ContactModels
+        {
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Comment is required")]
+        public string Comment { get; set; }
+    }
+    }
