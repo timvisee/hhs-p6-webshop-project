5f772bed63b4e9b21a03e5b3e1a20aecfc3d8e7d
Tim Visée <timvisee@gmail.com>
2017-01-17 12:55:10 +0100

    Create Program class tests

---
+++ hhs-p6-webshop-project-test/ProgramTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+using Program = hhs_p6_webshop_project.Program;
+
+namespace hhs_p6_webshop_project_test {
+    public class ProgramTests {
+
+        [Fact]
+        public void ShouldHaveAppName() {
+            // Application name should not be empty
+            Assert.NotEmpty(Program.APP_NAME);
+        }
+
+        [Fact]
+        public void ShouldHaveVersionName() {
+            // Application name should not be empty
+            Assert.NotEmpty(Program.APP_VERSION_NAME);
+        }
+
+        [Fact]
+        public void ShouldHaveVersionCode() {
+            // Application version code should be specified and above zero
+            Assert.True(Program.APP_VERSION_CODE > 0);
+        }
+
+    }
+}
