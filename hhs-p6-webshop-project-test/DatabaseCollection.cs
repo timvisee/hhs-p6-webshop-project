ec44df5b658f80a33e2be0e183f95499b2ae5993
Tim Visée <timvisee@gmail.com>
2017-01-17 14:24:25 +0100

    Create database collection and fixture, try to use the application database context (breaking

---
+++ hhs-p6-webshop-project-test/DatabaseCollection.cs
@@ -0,0 +1,7 @@
+﻿using Xunit;
+
+namespace hhs_p6_webshop_project_test {
+
+    [CollectionDefinition("main")]
+    public class DatabaseCollection : ICollectionFixture<DatabaseFixture> { }
+}
---
+++ hhs-p6-webshop-project-test/DemoTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace hhs_p6_webshop_project_test {
+
+    [Collection("main")]
+    public class DemoTests {
+
+        /// <summary>
+        /// Database fixture instance, used for testing.
+        /// </summary>
+        private DatabaseFixture _databaseFixture;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="databaseFixture">Database fixture.</param>
+        public DemoTests(DatabaseFixture databaseFixture) {
+            this._databaseFixture = databaseFixture;
+        }
+
+        [Fact]
+        public void SampleTest() {
+            Console.WriteLine("GETTING DATA FROM DB: ");
+//            Console.WriteLine(this._databaseFixture.Context.Appointment.ToList());
+        }
+    }
+}
