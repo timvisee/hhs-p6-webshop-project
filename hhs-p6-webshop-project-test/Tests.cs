86bd439d4742973c291c029fe1247f9d2d378caa
Tim Visée <timvisee@gmail.com>
2017-01-17 12:45:14 +0100

    Create new test project for Xunit, included main project

--- hhs-p6-webshop-project-test/Tests.cs
+++ hhs-p6-webshop-project-test/Tests.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using NUnit.Framework;
+using Xunit;
 
 namespace hhs_p6_webshop_project_test {
-    [TestFixture]
     public class Tests {
-        [Test]
+
+        [Fact]
         public void Test1() {
             Assert.True(true);
         }
+
     }
 }
