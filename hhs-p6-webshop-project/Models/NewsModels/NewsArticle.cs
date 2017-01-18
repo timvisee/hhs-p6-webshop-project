c890188eb74bdfd25c25b7800a88a24bda5fe775
Miladintjuh <miladin@live.nl>
2017-01-18 19:10:48 +0100

    Implemented basic many 2 many models for news

---
+++ hhs-p6-webshop-project/Models/NewsModels/NewsArticle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace hhs_p6_webshop_project.Models.NewsModels
+{
+    public class NewsArticle
+    {
+        public NewsArticle()
+        {
+            NewsArticleCategories = new List<NewsArticleCategory>();
+        }
+
+        public int NewsArticleID { get; set; }
+        public string Name { get; set; }
+
+        [Display(Name = "NewsArticleCategories")]
+        public virtual ICollection<NewsArticleCategory> NewsArticleCategories { get; set; }
+    }
+
+    public class NewsArticleVM
+    {
+        public NewsArticle NewsArticle { get; set; }
+
+        [Display(Name = "Studenten")]
+        public IEnumerable<SelectListItem> NewsCategoriesList { get; set; }
+
+        [Display(Name = "Studenten")]
+        public IEnumerable<int> SelectedNewsCategories { get; set; }
+    }
+}
---
+++ hhs-p6-webshop-project/Models/NewsModels/NewsArticleCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hhs_p6_webshop_project.Models.NewsModels
+{
+    public class NewsArticleCategory
+    {
+        [ForeignKey("NewsArticle")]
+        public int NewsArticleID { get; set; }
+        public virtual NewsArticle NewsArticle { get; set; }
+
+        [ForeignKey("NewsCategory")]
+        public int NewsCategoryID { get; set; }
+        public virtual NewsCategory NewsCategory { get; set; }
+    }
+}
