904d1e269c1d8d30dd4ddacd8d09eeceb3c5b9b0
Tim Visée <timvisee@gmail.com>
2017-01-21 23:10:42 +0100

    Create database context test

---
+++ hhs-p6-webshop-project-test/DatabaseContextTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace hhs_p6_webshop_project_test {
+
+    [Collection("main")]
+    public class DatabaseContextTests {
+
+        /// <summary>
+        /// Database fixture instance, used for testing.
+        /// </summary>
+        private DatabaseFixture _databaseFixture;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="databaseFixture">Database fixture.</param>
+        public DatabaseContextTests(DatabaseFixture databaseFixture) {
+            this._databaseFixture = databaseFixture;
+        }
+
+        /// <summary>
+        /// Ensure the database test context isn't null.
+        /// </summary>
+        [Fact]
+        public void ValidateDatabaseContext() {
+            Assert.NotNull(this._databaseFixture.Context);
+        }
+    }
+}
