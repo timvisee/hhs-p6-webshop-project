b6968758e11cf62a524106e824cd77b015840884
Tim Visée <timvisee@gmail.com>
2017-01-22 17:17:01 +0100

    Deploy on Linux using password

--- ci/linux/deploy.sh
+++ ci/linux/deploy.sh
@@ -10,11 +10,8 @@ log() {
 # Deploy the product
 log "Starting Linux deployment process..."
 
-log "Adding deployment key..."
-echo "$LINUX_DEPLOY_KEY" >> ~/.ssh/authorized_keys
-
 log "Deploying site..."
-ssh -v -o StrictHostKeyChecking=no root@honeymoon.timvisee.com "~/updateSite"
+sshpass -p "$LINUX_DEPLOY_PASS" ssh -v -o StrictHostKeyChecking=no root@honeymoon.timvisee.com "~/updateSite"
 
 # Show a success message
 log "Site successfully deployed.\nURL: http://honeymoon.timvisee.com/"
