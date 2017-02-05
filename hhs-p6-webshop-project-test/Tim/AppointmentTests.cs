0b7cfa808abff05e4977ba9f83d586abb79e20cf
timvisee <timvisee@gmail.com>
2017-02-05 23:00:38 +0100

    Create a test for the GetTimes method

--- hhs-p6-webshop-project-test/Tim/AppointmentTests.cs
+++ hhs-p6-webshop-project-test/Tim/AppointmentTests.cs
@@ -19,18 +19,28 @@ namespace hhs_p6_webshop_project_test.Tim {
         /// <summary>
         /// A date that is fully booked.
         /// </summary>
-        private static readonly String FULL_DATE = "2017-01-26";
+        private static readonly String DATE_FULL = "2017-01-26";
+
+        /// <summary>
+        /// A date that is partially booked.
+        /// </summary>
+        private static readonly String DATE_PARTIAL = "2017-01-24";
+
+        /// <summary>
+        /// A date that isn't booked at all.
+        /// </summary>
+        private static readonly String DATE_EMPTY = "2017-01-27";
 
         /// <summary>
         /// Constant list of dates used for testing purposes.
         /// </summary>
         static readonly List<DateTime> TestDates = new List<DateTime>()
         {
-            DateTime.Parse("2017-01-24 09:00:00"),
+            DateTime.Parse(DATE_PARTIAL + " 09:00:00"),
             DateTime.Parse("2017-01-25 12:30:00"),
-            DateTime.Parse(FULL_DATE + " 09:00:00"),
-            DateTime.Parse(FULL_DATE + " 12:30:00"),
-            DateTime.Parse(FULL_DATE + " 15:00:00"),
+            DateTime.Parse(DATE_FULL + " 09:00:00"),
+            DateTime.Parse(DATE_FULL + " 12:30:00"),
+            DateTime.Parse(DATE_FULL + " 15:00:00"),
         };
 
         /// <summary>
@@ -76,7 +86,7 @@ namespace hhs_p6_webshop_project_test.Tim {
                 Assert.IsType<String>(dateString);
 
                 // Make sure this date equals the full date
-                Assert.Equal(dateString, FULL_DATE);
+                Assert.Equal(dateString, DATE_FULL);
 
                 // Increase the count
                 count++;
@@ -84,6 +94,43 @@ namespace hhs_p6_webshop_project_test.Tim {
 
             // Assert the count
             Assert.Equal(count, 1);
+        }
+
+        /// <summary>
+        /// Test whether the response given by the API to get the unavailable dates works.
+        /// </summary>
+        [Fact]
+        public void GetTimesTest()
+        {
+            // Set up the mock for the service
+            var mockAppointmentService = new Mock<IAppointmentService>();
+            mockAppointmentService.Setup(a => a.GetAllAppointments()).Returns(CreateTestAppointments);
+
+            // Get the mocked appointment API controller
+            var appointmentApiController = new AppointmentApiController(mockAppointmentService.Object);
+
+            // Get the unavailable dates
+            var timesResponse = appointmentApiController.GetTimes(DATE_FULL);
+
+            // Make sure the response is of the correct type
+            Assert.IsType<JsonResult>(timesResponse);
+
+            // Get the list of times as dynamic object
+            var times = timesResponse.Value as dynamic;
+
+            // Loop through the values, keep track of the count
+            int timeCount = 0;
+            foreach (var timeObject in times)
+            {
+                // Make sure the time is occupied
+                Assert.False(timeObject.available);
+
+                // Increase the time count
+                timeCount++;
+            }
+
+            // Assert the count
+            Assert.Equal(timeCount, 3);
         }
     }
 }
