d148a45cd52b3d81153f9fc60acfb4f15dcf22f1
TheBauwssss <github@rick-soft.com>
2017-02-05 16:07:59 +0100

    Removed useless code from DressFinderController

--- hhs-p6-webshop-project/Api/DressFinderController.cs
+++ hhs-p6-webshop-project/Api/DressFinderController.cs
@@ -80,31 +80,6 @@ namespace hhs_p6_webshop_project.Api
         {
             return Json(ProductService.GetAllProductsPaged(start, count));
         }
-
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] {"value1", "value2"};
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        
     }
 }
